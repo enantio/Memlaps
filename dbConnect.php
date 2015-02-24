1e9825535d0496e59a44ec5365997b824e58e2dd
enantio <emory-k@hotmail.com>
2015-02-24 01:56:01 -0600

    included for reference: dbConnect.php and preprocessing.js

---
+++ dbConnect.php
@@ -0,0 +1,8 @@
+<?php
+        define("HOST","");//mysql server info must be added here
+        define("USER","");
+        define("PASS","");
+        define("DBNAME","");
+       
+        $DBconnection=mysqli_connect(HOST,USER,PASS,DBNAME);
+  ?>
--- index.php
+++ index.php
@@ -7,7 +7,7 @@
     
     <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script> 
     <script src="js/bootstrap.js"></script>
-
+	<script src="preprocessing.js"></script>
     
   </head>
   <body>
@@ -35,13 +35,13 @@
 	<div class ="navbar navbar-inverse navbar-static-top">
 		<div class = "container">
             
-            <ul class=" nav navbar-nav navbar-left ">
-			    <li><a href = "#" class = "">Blank Page</a></li>
+            <ul class=" nav navbar-nav navbar-left "> <!--change this list to horizontal tabs (not hrefs)-->
+			    <li><a href="#" class="">Blank Page</a></li>
                 <li><a href="#" class="">Tutorial</a></li>
                 <li><a href="#" class="">File Name</a></li>
             </ul> 
 
-            <ul class=" nav navbar-nav navbar-right">
+            <ul class=" nav navbar-nav navbar-right"> <!--css righ positioned-->
                 <li class="dropdown">
                     <a class="dropdown-toggle" data-toggle="dropdown" href="#">
                         My Account
@@ -49,8 +49,8 @@
                     </a>
                     <ul class="dropdown-menu">
                         <li><a href="MyProfile.php?username=<?php include('displayUN.php');?>">My Profile</a></li>
-                        <li><a href="#">My Files</a></li>
-                        <li><a href="memlapsSignIn.html">Logout</a></li>
+                        <li><a href="#">My Files</a></li> <!--display prompt? accordian js?-->
+                        <li><a href="memlapsSignIn.html">Logout</a></li> <!--and call script to log out-->
                     </ul>
              </ul>
 
@@ -60,7 +60,7 @@
     
     <div><!--main note div-->
 		<form action="index.php?username=<?php include('displayUN.php');?>" method="POST"/>
-			<textarea cols="150" rows="25" name="noteText"><?php include('noteDisplay.php'); ?></textarea>
+			<textarea cols="150" rows="25" name="noteText"><?php include('noteDisplay.php'); ?>Start Typing Here</textarea>
 			</br><h4>Title:<h4>
 			<input type="text" name="title" value="<?php include('titleDis.php'); ?>"/>
 			</br><h4>Comments:<h4>
