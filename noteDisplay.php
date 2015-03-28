52ce3c0cc8993780a671ad05aaea69de8a05d867
johnfoyt <johnfoyt0324@gmail.com>
2015-03-27 20:44:51 -0700

    Update noteDisplay.php

--- noteDisplay.php
+++ noteDisplay.php
@@ -1,6 +1,7 @@
 <?php 
 	 if(isset($_GET['title'])){
-		$statement="select notes from Notes where author='". $_GET['username']."' and title='". $_GET['title']."';";
+		if(!isset($_GET['author']))$statement="select notes from Notes where author='". $_GET['username']."' and title='". $_GET['title']."';";
+		else $statement="select notes from Notes where author='". $_GET['author']."' and title='". $_GET['title']."';";
 		$note=mysqli_query($DBconnection,$statement);
 		$dataRow=mysqli_fetch_array($note,MYSQL_BOTH);
 		echo $dataRow['notes'];
