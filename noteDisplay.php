b9ae1c3e2943760093e3ceb96c07af7df2f4f19d
johnfoyt <johnfoyt0324@gmail.com>
2015-03-28 21:38:33 -0700

    Update noteDisplay.php

--- noteDisplay.php
+++ noteDisplay.php
@@ -1,6 +1,6 @@
 <?php 
 	 if(isset($_GET['title'])){
-		if(!isset($_GET['author']))$statement="select notes from Notes where author='". $_GET['username']."' and title='". $_GET['title']."';";
+		if(!isset($_GET['author']))$statement="select notes from Notes where author='". $_GET['username']."' and title='".$_GET['title']."';";
 		else $statement="select notes from Notes where author='". $_GET['author']."' and title='". $_GET['title']."';";
 		$note=mysqli_query($DBconnection,$statement);
 		$dataRow=mysqli_fetch_array($note,MYSQL_BOTH);
@@ -8,7 +8,8 @@
 		mysqli_free_result($note);
 	 }
 	 elseif(isset($_POST['title'])){
-		$statement="select notes from Notes where author='". $_GET['username']."' and title='". $_POST['title']."';";
+		if(!isset($_GET['author']))$statement="select notes from Notes where author='". $_GET['username']."' and title='".$_POST['title']."';";	
+		else $statement="select notes from Notes where author='". $_GET['author']."' and title='". $_POST['title']."';";
 		$note=mysqli_query($DBconnection,$statement);
 		$dataRow=mysqli_fetch_array($note,MYSQL_BOTH);
 		echo $dataRow['notes'];
