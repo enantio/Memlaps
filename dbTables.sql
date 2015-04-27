a0c3287477d465332b5cc57ed0c66a46381ec2d3
johnfoyt <johnfoyt0324@gmail.com>
2015-04-26 22:54:17 -0700

    Update dbTables.sql
    fix to note share primary key

--- dbTables.sql
+++ dbTables.sql
@@ -4,5 +4,5 @@ DROP TABLE IF EXISTS Friends;
 DROP TABLE IF EXISTS User_Info;
 CREATE TABLE User_Info(username CHAR(30) PRIMARY KEY,email CHAR(40), name CHAR(20), password_hash CHAR(128));
 CREATE TABLE Notes(title CHAR(25), author CHAR(30), comments CHAR(50), notes TEXT,last_mod CHAR(40), meta_data CHAR(50), PRIMARY KEY(title,author),FOREIGN KEY (author) REFERENCES User_Info(username));
-CREATE TABLE Note_Share(author CHAR(30),title CHAR(25),share_W_user CHAR(30), PRIMARY KEY(author,title),FOREIGN KEY (author) REFERENCES User_Info(username),FOREIGN KEY (share_W_user) REFERENCES User_Info(username));
+CREATE TABLE Note_Share(author CHAR(30),title CHAR(25),share_W_user CHAR(30), PRIMARY KEY(author,title,share_W_user),FOREIGN KEY (author) REFERENCES User_Info(username),FOREIGN KEY (share_W_user) REFERENCES User_Info(username),FOREIGN KEY (title) REFERENCES Notes(title));
 CREATE TABLE Friends(username CHAR(30),friend CHAR(30),PRIMARY KEY (username,friend),FOREIGN KEY (username) REFERENCES User_Info(username),FOREIGN KEY (friend) REFERENCES User_Info(username));
