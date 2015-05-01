35589246f02941e740069045e9c5819ea72408c3
enantio <enantio@users.noreply.github.com>
2015-05-01 14:51:46 -0500

    Update index.php
    deleted unnecessary tutorial tab.
    added green background color

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+
 <!DOCTYPE html>
 <html>
   <head>
@@ -13,66 +14,39 @@
     <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script> 
     <script src="js/bootstrap.js"></script>
 
+	<style>
+		body {
+		background-color: #33CC33;
+		}
+	</style>
     
   </head>
   <body>
-	<?php
-		include('dbConnect.php');
-		if(isset($_POST['noteText'])){
-			if(!isset($_GET['author']))$author=$_POST['username'];
-			else $author=$_GET['author'];
-
-			$statement="select * from Notes where author='".$author."' and title='". $_POST["title"]."';";
-			
-			$note=mysqli_query($DBconnection,$statement);
-			$dataRow=mysqli_fetch_array($note,MYSQL_BOTH);
-
-			 
-			//check for uploaded file
-			if(isset($_FILES['fileToUpload'])){
-				include('OCRnoteAdd.php');			
-			}
-			else $savedNotes=$_POST['noteText'];
-
-			if(!$dataRow){//check for existing note page
-				mysqli_free_result($note);
-				$statement="INSERT INTO Notes VALUES('".$_POST['title']."','".$author."','".$_POST['comments']."','".$savedNotes."','".date("r")."','meta stuff');";
-				mysqli_query($DBconnection,$statement);
-			}
-			else{
-				mysqli_free_result($note);
-				$statement="UPDATE Notes SET notes='".$savedNotes."', title='".$_POST['title']."', comments='".$_POST['comments']."' WHERE author='".$author."' and title='". $_POST['title']."';";
-				mysqli_query($DBconnection,$statement);
-			}
-		}
-    ?>
-	<div class ="navbar navbar-inverse navbar-static-top"> <!--Navigation Bar -->
+	
+	<div class ="navbar navbar-inverse navbar-static-top"> 
+		<!--Navigation Bar -->
 		<div class = "container" role = "tabpanel">
             
             <ul class=" nav navbar-nav" role = "tablist">
-			    <li role="presentation" <?php if(!isset($_POST["title"]) && !isset($_GET["title"])):?> class = "active" <?php endif; ?>  ><a href = "#BlankPage"  aria-controls="BlankPage" role="tab" data-toggle="tab">Blank Page</a></li>  <!--Creates a Blank Page. If it's save it is sent to another tab--> 
+			    <li role="presentation"  class = "active"   ><a href = "#BlankPage"  aria-controls="BlankPage" role="tab" data-toggle="tab">Blank Page</a></li>  <!--Creates a Blank Page. If it's save it is sent to another tab--> 
 				<li role="presentation"><a href="#Tutorial"  aria-controls="Tutorial" role="tab" data-toggle="tab"> Tutorial</a></li> 
-                <?php if(isset($_POST["title"]) || isset($_GET["title"]))  :?><li role="presentation" class = "active"><a href="#FileName"  aria-controls="FileName" role="tab" data-toggle="tab"><?php include('titleDis.php'); ?></a></li> <!--If a file is opened it creates a new tab-->
-				<?php endif; ?> 
+                 
 			</ul> 
 	
 			
             <ul class=" nav navbar-nav navbar-right">
-              <?php if(isset($_POST["username"]) || isset($_GET["username"])) : ?>  <!--Checks to see if signed in-->
+                <!--Checks to see if signed in-->
 					<li class="dropdown">
                     <a class="dropdown-toggle" data-toggle="dropdown" href="#">
                         My Account
                         <span class="caret"></span>
                     </a>
                     <ul class="dropdown-menu">
-                        <li><a href="MyProfile.php?username=<?php include('displayUN.php');?>">My Profile</a></li>
+                        <li><a href="MyProfile.php?username=">My Profile</a></li>
                         <li><a href="#">My Files</a></li>
                         <li><a href="index.php">Logout</a></li>
                     </ul>
-				 <?php else :?> 
-					<li><a href = "memlapsSignIn.php" >Sign In</a></li>
-					<li><a href ="memlapsSignUp.php">Sign Up</a></li>
-				 <?php endif; ?> 
+				  
 				
              </ul>
 	
@@ -82,54 +56,47 @@
    <div class = "tab-content container" >
    
 		<!--Saved Notes Tab-->
-		<div role="tabpanel" class="tab-pane  <?php if(isset($_POST["title"]) || isset($_GET["title"])):?>active<?php endif; ?> " id="FileName">
-			<?php if(!isset($_GET['author'])) :?>
-				<form action="index.php?username=<?php include('displayUN.php');?>" method="POST" enctype="multipart/form-data"/>
-			<?php else:?>
-				<form action="index.php?username=<?php include('displayUN.php'); echo "&author=".$_GET['author'];?>" method="POST" enctype="multipart/form-data"/>
-			<?php endif; ?>
-					
+		<div role="tabpanel" class="tab-pane   " id="FileName">
+							<form action="index.php?username=" method="POST" enctype="multipart/form-data"/>
+								
 					<h4>Title:<h4>
-					<input type="text" name="title" value="<?php include('titleDis.php'); ?>"/>
-					</br><textarea cols="150" rows="25" name="noteText"><?php include('noteDisplay.php'); ?></textarea>
+					<input type="text" name="title" value=""/>
+					</br><textarea cols="150" rows="25" name="noteText"></textarea>
 					</br><h4>Comments:<h4>
-					<input type="text" name="comments" value="<?php include('commentDis.php'); ?>"/>
+					<input type="text" name="comments" value=""/>
 					<br/>
 					<h4>Upload a picture of some text (must be a .png):<h4>
 					<input type="file" name="fileToUpload" accept="image/png" id="fileToUpload"/>
 					<br/>
-					<input type="hidden" name="username" value="<?php include('displayUN.php');?>"/>
+					<input type="hidden" name="username" value=""/>
 					<br/>	
 					<input type="submit" value="save"/>
 				</form>
 		</div>
 		
 		<!--Blank Page Tab-->
-		<div role="tabpanel" class="tab-pane  <?php if(!isset($_POST["title"]) && !isset($_GET["title"])):?> active <?php endif; ?>"id="BlankPage">
-			<?php if(!isset($_GET['author'])) :?>
-				<form action="index.php?username=<?php include('displayUN.php');?>" method="POST"  enctype="multipart/form-data" />
-			<?php else:?>
-				<form action="index.php?username=<?php include('displayUN.php'); echo "&author=".$_GET['author'];?>" method="POST" enctype="multipart/form-data"/>
-			<?php endif; ?>
-					<h4>Title:<h4>
+		<div role="tabpanel" class="tab-pane   active "id="BlankPage">
+							<form action="index.php?username=" method="POST"  enctype="multipart/form-data" />
+								<h4>Title:<h4>
 					<input type="text" name="title" />
-					</br><textarea cols="150" rows="25" name="noteText"></textarea>
-					</br><h4>Comments:<h4>
-					<input type="text" name="comments"/>
+					</br>
+					
+					<!--THE MAIN TEXTBOX-->
+					</br><textarea cols="50" rows="25" name="noteText"></textarea>
+					
+					</br><h4>Comments on this note:<h4>
+					<input type="text" cols="150" rows="3" name="comments"/>
 					<br/>
-					<h4>Upload a picture of some text (must be a .png):<h4>
+					<h4>Upload a picture of text (must be .png file):<h4>
 					<input type="file" name="fileToUpload" accept="image/png" id="fileToUpload"/>
 					<br/>
-					<input type="hidden" name="username" value="<?php include('displayUN.php');?>"/>
+					<input type="hidden" name="username" value=""/>
 					<br/>	
-					<input type="submit" value="save"/>
+					<input type="submit" value="Save"/>
 				</form>
 		</div>
 	
-		<!--Tutorial Tab-->
-		<div role="tabpanel" class="tab-pane" id="Tutorial">
-			<h3>Tutorial</h3>
-		</div>
+		
 		
 	</div>
 	
