03cd77b4b6da01e7d09362d5dec0d12023de38f5
enantio <enantio@users.noreply.github.com>
2015-05-04 17:28:26 -0500

    Create bgroundClasses.js
    replaces keys.js. most important script.

---
+++ bgroundClasses.js
@@ -0,0 +1,137 @@
+$(document).ready(function(){
+
+$('body').addClass('bground9');
+$('.nicEdit-main').toggleClass('tground0');
+
+
+var map = [];
+window.addEventListener("keydown",
+    function(e){
+        map[e.keyCode] = true;
+        multiKeys(e);
+		},
+false);
+
+
+window.addEventListener('keyup',
+    function(e){
+        map[e.keyCode] = false;
+		},
+false);
+
+
+function multiKeys(e){
+if(map[17] && map[83]){ // ctrl+s = save	
+	checkTitleAndSave()
+    e.preventDefault();
+    return false;
+	}
+
+
+/*PAGE BACKGROUND CHANGER*/
+else if(map[17] && map[49]){ // ctrl+1 
+	$('body').removeClass();
+	$('body').addClass('bground1');
+	//newBground();
+    e.preventDefault();
+    return false;
+}
+
+else if(map[17] && map[50]){ // ctrl+2 
+	$('body').removeClass();
+	$('body').addClass('bground0');
+	//oldBground();
+    e.preventDefault();
+    return false;
+}
+
+/*TEXTAREA BACKGROUND CHANGER*/
+else if(map[17] && map[51]){ // ctrl+3 
+	$('body').removeClass();
+	$('body').addClass('bground3');
+	//newTground();
+    e.preventDefault();
+    return false;
+}
+
+else if(map[17] && map[52]){ // ctrl+4 
+	$('body').removeClass();
+	$('body').addClass('bground4');
+	//oldTground();
+    e.preventDefault();
+    return false;
+}
+else if(map[17] && map[53]){ // ctrl+5 
+	$('body').removeClass();
+	$('body').addClass('bground5');
+	//oldTground();
+    e.preventDefault();
+    return false;
+}
+else if(map[17] && map[54]){ // ctrl+6 
+	$('body').removeClass();
+	$('body').addClass('bground6');
+	//oldTground();
+    e.preventDefault();
+    return false;
+}
+else if(map[17] && map[55]){ // ctrl+7 
+	$('body').removeClass();
+	$('body').addClass('bground7');
+	//oldTground();
+    e.preventDefault();
+    return false;
+}
+else if(map[17] && map[56]){ // ctrl+8 
+	$('body').removeClass();
+	$('body').addClass('bground8');
+	//oldTground();
+    e.preventDefault();
+    return false;
+}
+else if(map[17] && map[57]){ // ctrl+9 
+	$('body').removeClass();
+	$('body').addClass('bground2');
+	//oldTground();
+    e.preventDefault();
+    return false;
+}
+
+else if(map[17] && map[48]){ // ctrl+0 
+	$('body').removeClass();
+	$('body').addClass('bground9');
+	//oldTground();
+    e.preventDefault();
+    return false;
+}
+
+else if(map[17] && map[81]){ // ctrl+ q
+	$('.nicEdit-main').toggleClass('tground1');
+    e.preventDefault();
+    return false;
+}
+
+}//end multiKeys
+
+
+
+});
+
+
+
+
+
+function checkTitleAndSave(){
+if ($('#entitled').val() === ""){
+		var title = prompt("Give this note a title", "");
+        if (title === ""){
+			//do not save;
+			alert("Note was not saved.");
+			
+		}
+        else {
+			$('#entitled').val(title);
+			$('form').submit();
+		}
+	} 
+}
