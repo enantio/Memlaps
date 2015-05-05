340ff630ce1bff65ef4feb46c2c3ebf971a1fce0
johnfoyt <johnfoyt0324@gmail.com>
2015-05-05 17:50:59 -0500

    Update addFriend.php
    new query security updates

--- addFriend.php
+++ addFriend.php
@@ -1,10 +1,19 @@
 <?php
 	if(isset($_POST['addAFriend'])){
-		$statement="SELECT * FROM Friends WHERE username='".$_GET["username"]."' AND friend='".$_POST['addAFriend']."';";
+		//check friends name and use secure copy from DBconnection
+		$statement=$DBconnection->prepare("select username from User_Info where username=?;");
+		$statement->bind_param('s',$_POST['addAFriend']);
+		$statement->execute();
+		$note=$statement->get_result();
+		$FriendsName=$note->fetch_assoc();
+		$statement->close();
+		
+		
+		$statement="SELECT * FROM Friends WHERE username='".$_GET["username"]."' AND friend='".$FriendsName['username']."';";
 		$friends=mysqli_query($DBconnection,$statement);
 		if(mysqli_num_rows($friends)==0){
 			mysqli_free_result($friends);
-			$statement="INSERT INTO Friends VALUES('".$_GET["username"]."','".$_POST['addAFriend']."');";
+			$statement="INSERT INTO Friends VALUES('".$_GET["username"]."','".$FriendsName['username']."');";
 			mysqli_query($DBconnection,$statement);
 		}
 		else{
