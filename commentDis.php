4a36f4d6b6616fa87fb81077dcc80892d2e72d61
johnfoyt <johnfoyt0324@gmail.com>
2015-05-05 17:52:16 -0500

    Update commentDis.php
    query security updates

--- commentDis.php
+++ commentDis.php
@@ -1,23 +1,21 @@
 <?php 
-	 if(isset($_GET['comments'])){
-		$statement="select comments from Notes where author='". $_GET["username"]."' and title='". $_GET["title"]."';";
-		$note=mysqli_query($DBconnection,$statement);
-		$dataRow=mysqli_fetch_array($note,MYSQL_BOTH);
+	if(!isset($_GET['title']) && !isset($_POST['title']))
+		echo"";
+	else{
+		$statement=$DBconnection->prepare("select comments from Notes where author=? and title=?;");
+		if(isset($_GET['comments'])){
+			$statement->bind_param('ss', $_GET['username'],$_GET['title']);
+		}
+		elseif(isset($_POST['comments'])){
+			$statement->bind_param('ss', $_GET['username'],$_POST['title']);
+		}
+		elseif(isset($_GET['author'])){
+			$statement->bind_param('ss', $_GET['author'],$_GET['title']);
+		}
+		$statement->execute();
+		$note=$statement->get_result();
+		$dataRow=$note->fetch_assoc();
 		echo $dataRow['comments'];
-		mysqli_free_result($note);
-	 }
-	 elseif(isset($_POST['comments'])){
-		$statement="select comments from Notes where author='". $_GET["username"]."' and title='". $_POST["title"]."';";
-		$note=mysqli_query($DBconnection,$statement);
-		$dataRow=mysqli_fetch_array($note,MYSQL_BOTH);
-		echo $dataRow['comments'];
-		mysqli_free_result($note);
-	}
-	elseif(isset($_GET['author'])){
-		$statement="select comments from Notes where author='". $_GET["author"]."' and title='". $_GET["title"]."';";
-		$note=mysqli_query($DBconnection,$statement);
-		$dataRow=mysqli_fetch_array($note,MYSQL_BOTH);
-		echo $dataRow['comments'];
-		mysqli_free_result($note);
+		$statement->close();
 	}
 ?>
